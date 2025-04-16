d7d23165f753cbf5c00a4faf618e444d4a6c12a2
Arjal <arjalshah@gmail.com>
2025-04-16 22:44:45 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,12 @@ import Tech from './Tech'
 import Info from './Info'
 export default function Home() {
   return (
-    <div className='py-2'>
+    <div className='py-2 max-sm:bg-amber-600'>
+      {/* <div className='h-[300px] w-[300px] border-2 animate-myAni'>
+
+      </div> */}
+      <button className='btn'>click</button>
+      <div className='h-[200px] w-[200px] border-2 flex-center'>hello</div>
       <Intro />
 
       <Tech />
--- src/pages/home/Tech.jsx
+++ src/pages/home/Tech.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { DiGithub, DiJava, DiJavascript, DiPython, DiReact } from 'react-icons/di'
+import { DiAndroid, DiAngularSimple, DiGithub, DiJava, DiJavascript, DiPython, DiReact } from 'react-icons/di'
 
 export default function Tech() {
   return (
-    <div className='grid grid-cols-4 justify-items-center'>
+    <div className='grid grid-cols-(--my-grid) justify-items-center'>
       <DiReact className='animate-bounce hover:text-red-600' size={200} />
       <DiPython className='hover:scale-110 ease-in transition duration-100' size={200} />
       <DiJavascript size={200} className='text-some' />
       <DiJava size={200} />
       <DiGithub size={200} />
+      <DiPython size={200} />
+      <DiAndroid size={200} />
+      <DiAngularSimple size={200} />
     </div>
   )
 }
