dd2ae858190695585953c9ecde39a7e58ff1d539
Arjal <arjalshah@gmail.com>
2025-05-02 22:55:19 +0545

    changes

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router'
 
-
 export default function RootLayout() {
   return (
-    <div className='min-h-screen flex flex-col max-w-[1500px] mx-auto'>
-      <main className='flex-1'>
-        <Header />
-        <Outlet />
-      </main>
-
+    <div>
+      <Header />
+      <Outlet />
     </div>
   )
 }
